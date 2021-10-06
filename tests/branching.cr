1afeb9ce4d4689e6c3181d082d8352827b102507
Aria Ghora Prabono <hello@ghora.net>
2021-10-06 11:06:27 +0700

    Tests: add `elif` sub-test

--- tests/branching.cr
+++ tests/branching.cr
@@ -2,7 +2,17 @@ x: int = 100
 y: int = 50
 
 if x > y:
-	println("foo")
+	println("foo") -- will be printed
 else:
 	println("bar")
+end
+
+if x < y:
+	println("foo")
+elif 1 + 1 == 3:
+	println("bar")
+elif 1 + 1 == 2:
+	println("baz") -- will be printed
+else:
+	println("should not be executed")
 end
